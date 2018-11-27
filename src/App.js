bf42645cfee8a4a02c9ddcff2cca28b40f2da138
Gabriel Ng <gabrielng15@gmail.com>
2018-11-26 22:28:11 -0800

    Added CDM and fetch call in app.js (#4)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class App extends Component {
+  state = {
+    investments: [],
+  };
+
+  componentDidMount = async () => {
+    let response = await fetch("https://gist.githubusercontent.com/cranium/d8b83184bf0750f2c834760b7c9203dc/raw/a73a70716951f77b90e84b8848ff1fee46938dd1/soi.json");
+    let json = await response.json();
+  }
+  
   render() {
     return (
       <div>
