0e9161eed29ffda52604648a5ae11e0299b9d4bc
Gabriel Ng <gabrielng15@gmail.com>
2018-11-28 14:59:58 -0800

    Updates

---
+++ precache-manifest.49757225fa8ec7bcfef664c8279d8ec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3317085c0aca96f6354",
+    "url": "/cis/static/css/main.409be5fa.chunk.css"
+  },
+  {
+    "revision": "c3317085c0aca96f6354",
+    "url": "/cis/static/js/main.c3317085.chunk.js"
+  },
+  {
+    "revision": "c9510aec44dd6e061b1f",
+    "url": "/cis/static/css/1.163aa885.chunk.css"
+  },
+  {
+    "revision": "c9510aec44dd6e061b1f",
+    "url": "/cis/static/js/1.c9510aec.chunk.js"
+  },
+  {
+    "revision": "0a4833d0be416f8e9687",
+    "url": "/cis/static/js/runtime~main.0a4833d0.js"
+  },
+  {
+    "revision": "4d5c78fb153ef7a9f5a812a063aa1e8f",
+    "url": "/cis/index.html"
+  }
+];
